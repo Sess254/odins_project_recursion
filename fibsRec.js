3dcbce7ffa712561a2ec2026235e279d4a737bac
Sess254 <wamaritemoses@gmail.com>
2025-06-09 15:51:15 +0300

    fib recursive works

--- fibsRec.js
+++ fibsRec.js
@@ -1,11 +1,21 @@
- #!/usr/bin/env node
+#!/usr/bin/env node
 
-function fibsRec(n) {
-	if (n < 2) {
-		return n
-	}
-	
-	return fibsRec(n - 1) + fibsRec(n - 2)
+function fibs(n) {
+	if (n < 2) return n
+	return fibs(n - 1) + fibs(n - 2)
 }
 
+function fibsRec(n) {
+	if (n <= 0) return []
+	if (n === 1) return [0]
+	if (n === 2) return [0, 1] 
 
+	const prev = fibsRec(n - 1)
+	return [...prev, fibs(prev.length - 1) + fibs(prev.length - 2)]
+	
+}
+
+const args = process.argv.slice(2)
+const n = parseInt(args[0], 10)
+
+console.log(fibsRec(n))
