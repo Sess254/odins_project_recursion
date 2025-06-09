a40ebf89de6e6fd27a5b8558494664826cfdf1be
Sess254 <wamaritemoses@gmail.com>
2025-06-10 00:20:46 +0300

    made mergesort using recursion

--- fibsRec.js
+++ fibsRec.js
@@ -6,6 +6,7 @@ function fibs(n) {
 }
 
 function fibsRec(n) {
+	console.log("This was printed recursively")
 	if (n <= 0) return []
 	if (n === 1) return [0]
 	if (n === 2) return [0, 1] 
---
+++ mergSort.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const merge = (leftArr, rightArr) => {
+    const outPut = []
+    let leftIndex = 0
+    let rightIndex = 0
+
+
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+        const leftEl = leftArr[leftIndex]
+        const rightEl = rightArr[rightIndex]
+
+        if (leftEl < rightEl) {
+            outPut.push(leftEl)
+            leftIndex++
+        } else {
+            outPut.push(rightEl)
+            rightIndex++
+        }
+    }
+
+    return [...outPut, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)]
+
+}
+
+const mergeSort = array => {
+    if (array.length <= 1) return array
+
+    const middleIndex = Math.floor(array.length / 2)
+    const leftArr = array.slice(0, middleIndex)
+    const rightArr = array.slice(middleIndex)
+
+    return merge(mergeSort(leftArr), mergeSort(rightArr))
+}
+
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]))
+console.log(mergeSort([105, 79, 100, 110]))
